51306782630f1f7d8dfcba33f50856dbd7f8e311
Felicia Giza <57772642+feliciacricri@users.noreply.github.com>
2026-03-08 09:55:45 +0200

    implementare clasa student si meniu pentru interfata cu utilizatorul

---
+++ EvidentaStudenti/Program.cs
@@ -0,0 +1,106 @@
+﻿using LibrarieModele;
+
+namespace EvidentaStudenti
+{
+    class Program
+    {
+        public static void Main()
+        {
+            List<Student> studenti = new List<Student>();
+            Student? studentNou = null;
+            string optiune;
+
+            do
+            {
+                Console.WriteLine("C. Citire informatii student de la tastatura");
+                Console.WriteLine("I. Afisarea informatiilor despre ultimul student introdus");
+                Console.WriteLine("A. Afisare studenti din lista");
+                Console.WriteLine("S. Salvare student in lista");
+                Console.WriteLine("X. Inchidere program");
+
+                Console.WriteLine("Alegeti o optiune");
+                optiune = Console.ReadLine()?.ToUpper() ?? string.Empty;
+
+                switch (optiune)
+                {
+                    case "C":
+                        studentNou = CitireStudentTastatura();
+                        break;
+
+                    case "I":
+                        AfisareStudent(studentNou);
+                        break;
+
+                    case "A":
+                        AfisareStudenti(studenti);
+                        break;
+
+                    case "S":
+                        studentNou.IdStudent = studenti.Count + 1;
+                        studenti.Add(studentNou);
+                        Console.WriteLine("Student salvat.");
+                        break;
+
+                    case "X":
+                        Console.WriteLine("Aplicatia va fi inchisa");
+                        return;
+
+                    default:
+                        Console.WriteLine("Optiune inexistenta");
+                        break;
+                }
+
+            } while (optiune.ToUpper() != "X");
+
+            Console.ReadKey();
+        }
+
+        public static Student CitireStudentTastatura()
+        {
+            Console.WriteLine("Introduceti numele");
+            string nume = Console.ReadLine();
+
+            Console.WriteLine("Introduceti prenumele");
+            string prenume = Console.ReadLine();
+
+            Student student = new Student(0, nume, prenume);
+
+            Console.WriteLine("Introduceti numarul de note:");
+            int.TryParse(Console.ReadLine(), out int nrNote);
+
+            int[] note = new int[nrNote];
+            for (int i = 0; i < nrNote; i++)
+            {
+                Console.Write($"Nota {i + 1}: ");
+                bool rezultat = int.TryParse(Console.ReadLine(), out int nota);
+                if (rezultat)
+                {
+                    note[i] = nota;
+                }
+                else
+                {
+                    Console.WriteLine("Nota invalida, se va seta la 0.");
+                    note[i] = 0;
+                }
+            }
+            student.SetNote(note);
+
+            return student;
+        }
+
+        public static void AfisareStudent(Student student)
+        {
+            Console.WriteLine(student.Info());
+        }
+
+        public static void AfisareStudenti(List<Student> studenti)
+        {
+            Console.WriteLine("Studentii sunt:");
+
+            foreach (Student student in studenti)
+            {
+                AfisareStudent(student);
+            }
+        }
+    }
+}
---
+++ LibrarieModele/Student.cs
@@ -0,0 +1,57 @@
+﻿namespace LibrarieModele
+{
+    public class Student
+    {
+        //constante
+        private const char SEPARATOR = ' ';
+
+        // data membră privată
+        private int[] note;
+
+        // proprietăți auto-implemented
+        public int IdStudent { get; set; } // identificator unic student
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+
+        public void SetNote(int[] _note)
+        {
+            note = new int[_note.Length];
+            _note.CopyTo(note, 0);
+        }
+
+        public int[] GetNote()
+        {
+            // returnează o copie pentru a proteja datele interne
+            return (int[])note.Clone();
+        }
+
+        // constructor implicit
+        public Student()
+        {
+            Nume = string.Empty;
+            Prenume = string.Empty;
+            note = new int[0];
+        }
+
+        // constructor cu parametri
+        public Student(int idStudent, string nume, string prenume)
+        {
+            IdStudent = idStudent;
+            Nume = nume;
+            Prenume = prenume;
+            note = new int[0];
+        }
+
+        public string Info()
+        {
+            string sNote = string.Empty;
+            if (note != null)
+            {
+                sNote = string.Join(SEPARATOR.ToString(), note);
+            }
+
+            string info = $"Id:{IdStudent} Nume:{Nume ?? "NECUNOSCUT"} Prenume:{Prenume ?? "NECUNOSCUT"}  Note: {sNote}";
+            return info;
+        }
+    }
+}
