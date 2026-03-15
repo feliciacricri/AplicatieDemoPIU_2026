00b022474a3f4cc41ea3d6c40d2c8aacba47d0fd
Felicia Giza <57772642+feliciacricri@users.noreply.github.com>
2026-03-15 22:20:48 +0200

    implementare salvare date in fisier text

--- LibrarieModele/Student.cs
+++ LibrarieModele/Student.cs
@@ -1,9 +1,21 @@
-﻿namespace LibrarieModele
+﻿using System.Collections;
+
+namespace LibrarieModele
 {
     public class Student
     {
         //constante
-        private const char SEPARATOR = ' ';
+        private const char SEPARATOR_PRINCIPAL_FISIER = ';';
+        private const char SEPARATOR_SECUNDAR_FISIER = ' ';
+        private const bool SUCCES = true;
+        public const int NOTA_MINIMA = 1;
+        public const int NOTA_MAXIMA = 10;
+
+        private const int ID = 0;
+        private const int NUME = 1;
+        private const int PRENUME = 2;
+        private const int NOTE = 3;
+
 
         // data membră privată
         private int[] note;
@@ -42,16 +54,73 @@
             note = new int[0];
         }
 
+        //constructor cu un singur parametru de tip string care reprezinta o linie dintr-un fisier text
+        public Student(string linieFisier)
+        {
+            string[] dateFisier = linieFisier.Split(SEPARATOR_PRINCIPAL_FISIER);
+
+            //ordinea de preluare a campurilor este data de ordinea in care au fost scrise in fisier prin apelul implicit al metodei ConversieLaSir_PentruFisier()
+            this.IdStudent = Convert.ToInt32(dateFisier[ID]);
+            this.Nume = dateFisier[NUME];
+            this.Prenume = dateFisier[PRENUME];
+            ExtrageNote(dateFisier[NOTE], SEPARATOR_SECUNDAR_FISIER);
+        }
+
+
         public string Info()
         {
             string sNote = string.Empty;
             if (note != null)
             {
-                sNote = string.Join(SEPARATOR.ToString(), note);
+                sNote = string.Join(SEPARATOR_SECUNDAR_FISIER.ToString(), note);
             }
 
             string info = $"Id:{IdStudent} Nume:{Nume ?? "NECUNOSCUT"} Prenume:{Prenume ?? "NECUNOSCUT"}  Note: {sNote}";
             return info;
+        }
+
+        public string ConversieLaSirPentruFisier()
+        {
+            string sNote = string.Empty;
+            if (note != null)
+            {
+                sNote = string.Join(SEPARATOR_SECUNDAR_FISIER.ToString(), note);
+            }
+
+            string obiectStudentPentruFisier = string.Format("{1}{0}{2}{0}{3}{0}{4}",
+                SEPARATOR_PRINCIPAL_FISIER,
+                IdStudent.ToString(),
+                (Nume ?? " NECUNOSCUT "),
+                (Prenume ?? " NECUNOSCUT "),
+                sNote);
+
+            return obiectStudentPentruFisier;
+        }
+
+
+        public void ExtrageNote(string sirNote, char delimitator = ' ')
+        {
+            List<int> listaNote = new List<int>();
+
+            foreach (var element in sirNote.Split(delimitator))
+            {
+                if (int.TryParse(element, out int nota) && ValideazaNota(nota) == SUCCES)
+                {
+                    listaNote.Add(nota);
+                }
+            }
+
+            note = listaNote.ToArray();
+        }
+
+        private bool ValideazaNota(int nota)
+        {
+            if (nota >= NOTA_MINIMA && nota <= NOTA_MAXIMA)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
---
+++ NivelStocareDate/IStocareData.cs
@@ -0,0 +1,14 @@
+﻿using LibrarieModele;
+
+namespace NivelStocareDate
+{
+    //definitia interfetei
+    public interface IStocareData
+    {
+        void AddStudent(Student s);
+        List<Student> GetStudenti();
+        Student GetStudent(int idStudent);
+        Student GetStudent(string nume, string prenume);
+        bool UpdateStudent(Student s);
+    }
+}
