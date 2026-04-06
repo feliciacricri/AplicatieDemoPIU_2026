7e5db1e1b4384f1e978acd52b3a279decd6001aa
Felicia Giza <57772642+feliciacricri@users.noreply.github.com>
2026-04-06 11:36:39 +0300

    adaugare nivel UI WPF

---
+++ NivelUIWPF/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using LibrarieModele;
+using NivelStocareDate;
+using System.Windows;
+
+namespace NivelUIWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            IStocareData adminStudenti = StocareFactory.GetAdministratorStocare();
+            List<Student> studenti = adminStudenti.GetStudenti();
+            lblNrStudenti.Content = $"Numar studenti: {studenti.Count}";
+            lblStudenti.Content = "Studenti:\n" + string.Join("\n", studenti.Select(s => $"{s.IdStudent}: {s.Nume} {s.Prenume}"));
+        }
+    }
+}
